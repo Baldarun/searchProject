f91fd5e28897a775779b6948f50e187c830a859d
Baldarun <jay-_-morris@hotmail.co.uk>
2016-11-16 17:53:08 +0000

    Changed isExplored method
    old code still there but commented out. ANY DEVIATION FROM THE PREVIOUS IS CAUSING SEG FAULTS FOR NO REASON. JESUS FUCK WHAT'S EVEN GOING ON.

--- Algorithm.cpp
+++ Algorithm.cpp
@@ -18,11 +18,19 @@ bool Algorithm::inBounds(std::vector<int> toCheck)
 //returns true if the input point has already been explored (is already in the explored vector)
 bool Algorithm::isExplored(std::vector<int> toCheck)
 {
+/*
     for (int i=0; i<fExplored.size(); i++)
     {
         if(toCheck[0]==fExplored[i][0] && toCheck[1]==fExplored[i][1]) return true;
     }
     return false;
+*/
+
+    if(fExplored.empty()==true) return false;
+    else if(fExplored.back()[0]==toCheck[0] && fExplored.back()[1]==toCheck[1]) return true;
+    else return false;
+    //return false;
+
 }
 
 //findStart returns the coordinates that the start point is at within the map
